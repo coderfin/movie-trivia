43a3a83ef1a2ea9b0c14db863052c6968722a0d5
Ron Peterson <ron.peterson@byu.edu>
2017-03-09 14:27:40 -0700

    styles

--- public/components/user-profile.js
+++ public/components/user-profile.js
@@ -1,34 +1,36 @@
 let vcUserProfile = Vue.component("user-profile", {
   template: `
-    <section>
-      <h2>{{ player.displayName }}</h2>
+    <section class="user-profile component center">
+      <h1>&nbsp;</h1>
       <img v-bind:src="player.photoUrl" />
-      <dl>
-        <dt>High Score</dt>
-        <dd>{{ player.highScore || "0" }}</dd>
-      </dl>
-      <dl>
-        <dt>Games Played</dt>
-        <dd>{{ player.gamesPlayed || "0" }}</dd>
-      </dl>
-      <dl>
-        <dt>Total Points</dt>
-        <dd>{{ player.totalPoints || "0" }}</dd>
-      </dl>
-      <dl>
-        <dt>Average Points / Game</dt>
-        <dd>{{ (player.totalPoints / player.gamesPlayed) || "-" }}</dd>
-      </dl>
+      <h2>{{ player.displayName }}</h2>
+      <section class="stats">
+        <dl>
+          <dd>{{ player.highScore || "0" }}</dd>
+          <dt>High Score</dt>
+        </dl>
+        <dl>
+          <dd>{{ player.gamesPlayed || "0" }}</dd>
+          <dt>Games Played</dt>
+        </dl>
+        <dl>
+          <dd>{{ player.totalPoints || "0" }}</dd>
+          <dt>Total Points</dt>
+        </dl>
+        <dl>
+          <dd>{{ (player.totalPoints / player.gamesPlayed) || "-" }}</dd>
+          <dt>Average Points / Game</dt>
+        </dl>
+      </section>
     </section>
   `,
-  props: ['id'],
+  props: ["id"],
   data: function () {
     return {
       player: {}
     };
   },
   mounted: function () {
-
     firebaseData
         .players
         .child(this.$props.id)
