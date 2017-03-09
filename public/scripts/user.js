04d7c42ad63e85bdd912d4e2e4f3e6c74b03b58e
Trevor <trevorhreed@gmail.com>
2017-03-08 18:54:02 -0700

    fixing issues with user

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,42 +1,45 @@
-const userPromise = firebase.auth().getRedirectResult().then((result) => {
-    let googleUser = result.user;
-    if (!googleUser) {
-        let provider = new firebase.auth.GoogleAuthProvider();
-        provider.addScope("profile");
-
-        firebase.auth().signInWithRedirect(provider);
-    }
-
-    return new Promise((resolve, reject) => {
-        let firebaseUser = firebaseData.players.child(googleUser.uid);
-        firebaseUser.on("value", (user) => {
-            if (user.exists()) {
-                let player = user.val();
-                
-                resolve({
-                    displayName: player.displayName,
-                    photoUrl: player.photoUrl,
-                    highScore: player.highScore,
-                    firebaseUser
-                });
-            } else {
-                firebaseUser.set({
-                    displayName: googleUser.displayName,
-                    photoUrl: googleUser.photoURL,
-                    highScore: 0
-                }, (error) => {
-                    if (error) {
-                        reject(error);
-                    } else {
+const userPromise = new Promise((resolve, reject)=>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            resolve(user);
+        }else{
+            firebase.auth().getRedirectResult().then((result) => {
+                googleUser = result.user;
+                if (!googleUser) {
+                    let provider = new firebase.auth.GoogleAuthProvider();
+                    //provider.addScope("profile");
+                    firebase.auth().signInWithRedirect(provider);
+                }
+                let firebaseUser = firebaseData.players.child(googleUser.uid);
+                firebaseUser.on("value", (user) => {
+                    if (user.exists()) {
+                        let player = user.val();
                         resolve({
+                            displayName: player.displayName,
+                            photoUrl: player.photoUrl,
+                            highScore: player.highScore,
+                            firebaseUser
+                        });
+                    } else {
+                        firebaseUser.set({
                             displayName: googleUser.displayName,
                             photoUrl: googleUser.photoURL,
-                            highScore: 0,
-                            firebaseUser
+                            highScore: 0
+                        }, (error) => {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve({
+                                    displayName: googleUser.displayName,
+                                    photoUrl: googleUser.photoURL,
+                                    highScore: 0,
+                                    firebaseUser
+                                });
+                            }
                         });
                     }
                 });
-            }
-        });
+            });
+        }
     });
-});
+})
