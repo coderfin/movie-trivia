80dc7a0a71671cf9a2b1156a2a49de9660755f42
Trevor <trevorhreed@gmail.com>
2017-03-09 10:17:15 -0700

    game list moved to home route

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,7 +1,6 @@
 let router = new VueRouter({
     routes: [
         { path: "/", name: "home", component: vcHomeScreen },
-        { path: "/game-list", name: "game-list", component: vcGameList },
         { path: "/game/:id", name: "game", component: vcGame, props: true },
         { path: "*", redirect: "/" }
     ]
