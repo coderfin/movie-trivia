f66957fb72fba13d378c211460090173fec56ed1
Trevor <trevorhreed@gmail.com>
2017-03-10 12:51:27 -0700

    fixing game round

--- public/components/game-round.js
+++ public/components/game-round.js
@@ -1,16 +1,18 @@
 let vcGameRound = Vue.component('game-round', {
     template: `
-        <div>
+        <section class="game-round component center">
 
-            <count-down :description="'Starting in...'" :seconds="5" @callback="startRound()"></count-down>
+            <count-down :description="'Starting in...'" :seconds="5" @callback="startRound()" key="game-round-timer"></count-down>
+            <count-down :description="'Time left...'" :seconds="roundDuration" @callback="stopRound()" key="game-round-timer2" v-if="started"></count-down>
 
             <section v-if="started">
+                <h2>Prompt: {{ round.prompt.Title }}</h2>
                 <label>
                     <span>Guess a movie</span>
-                    <input v-model="searchTerm" @keyup="makeSuggestions()" />
+                    <input v-model="searchTerm" @keyup="makeSuggestions()" @keyup.enter="makeGuess()" />
                 </label>
                 <div class="suggestions" v-if="suggestions && suggestions.length">
-                    <div class="suggestion" v-for="(suggestion, index) in suggestions">
+                    <div class="suggestion" v-for="(suggestion, index) in suggestions" @click="makeGuess(suggestion)">
                         {{ suggestion.Title }}
                     </div>
                 </div>
@@ -21,18 +23,22 @@ let vcGameRound = Vue.component('game-round', {
                 </div>
             </section>
 
-        </div>
+        </section>
     `,
     props: ['gameId'],
     data: function(){
         return {
             round: {},
+            roundDuration: 0,
             started: false,
             searchTerm: "",
             suggestions: []
         }
     },
     mounted: function(){
+        firebaseData.games.child(`${this.gameId}/details/roundDuration`).once("value", (data)=>{
+            this.roundDuration = data.val();
+        });
         this.roundRef = firebaseData.games.child(`${this.gameId}/rounds/current`);
         this.roundRef.on("value", (data)=>{
             this.round = data.val();
@@ -41,6 +47,9 @@ let vcGameRound = Vue.component('game-round', {
     methods: {
         startRound(){
             this.started = true;
+        },
+        stopRound(){
+            this.$emit("finished");
         },
         makeSuggestions(){
             movieData.search(this.searchTerm).then((res)=>{
